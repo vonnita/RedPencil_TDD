308bfec9cfe739841e866c9e7767a1f3474baab8
vonnita <jonesvonn@gmail.com>
2016-09-13 20:26:35 -0400

    second changes

--- src/com/pillar/redpencil/RedPencilTest.java
+++ src/com/pillar/redpencil/RedPencilTest.java
@@ -10,17 +10,26 @@ public class RedPencilTest {
 	@Before
 	public void setUp() throws Exception {
 	}
-/*
- * Testing if sale is activated by 
- * by price reduction 
- */
-	@Test
-	public void test() { 
-		Redpencil  promoRedpencil = new Redpencil();
 
-		 assertFalse(promoRedpencil.isActivated(.05,.31,10.0,10.0));
-		
-		
+	/*
+	 * Testing if sale is not activated by by no price reduction
+	 */
+	@Test
+	public void test() {
+		Redpencil promoRedpencil = new Redpencil();
+
+		assertFalse(promoRedpencil.isActivated(.05, .31, 10.0, 10.0));
+	}
+
+	/*
+	 * Testing if sale is activated by a price reduction of 5%
+	 * 
+	 */
+	@Test
+	public void test2() {
+		Redpencil promoRedpencil = new Redpencil();
+
+		assertTrue(promoRedpencil.isActivated(.05, .31, 9.5, 10.0));
 	}
 
 }
--- src/com/pillar/redpencil/Redpencil.java
+++ src/com/pillar/redpencil/Redpencil.java
@@ -3,8 +3,11 @@ package com.pillar.redpencil;
 public class Redpencil {
 
 	public boolean isActivated(double lowEndReduction, double highEndReduction, double discountPrice, double orginalPrice) {
-		
+		if(discountPrice<=orginalPrice-orginalPrice*lowEndReduction){
+		return true;
+	}else{
 		return false;
 	}
 
+	}
 }
